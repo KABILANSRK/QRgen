d5bf5a643df2fb771a0803c8a6758d07e63897f3
Kabilan SRK <kabilansrk@gmail.com>
2025-02-15 15:10:33 +0530

    Basic UI

--- QRgen/src/App.jsx
+++ QRgen/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
+import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Home />
     </>
-  )
+  );
 }
 
 export default App
