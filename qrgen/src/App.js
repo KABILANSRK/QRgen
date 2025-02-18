abbeae44288ec5f38524cc053533d1b1dffbe5d0
Kabilan SRK <kabilansrk@gmail.com>
2025-02-18 21:23:07 +0530

    Final Commit for Link to QR

---
+++ qrgen/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import QRGenerator from '../src/pages/QRgenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <QRGenerator />
+    </div>
+  );
+}
+
+export default App;
